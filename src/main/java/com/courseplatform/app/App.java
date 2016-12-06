00bf53562d1c6974ed088615d1d180f9b23bb9ed
ricado <1169833934@qq.com>
2016-12-06 13:30:01 +0800

    项目courseplatform 课程平台

---
+++ src/main/java/com/courseplatform/app/App.java
@@ -0,0 +1,8 @@
+package com.courseplatform.app;
+/**
+*@author ye
+*@dete 2016年12月6日 下午1:24:13
+*/
+public class App {
+
+}
---
+++ src/test/java/com/courseplatform/app/AppTest.java
@@ -0,0 +1,8 @@
+package com.courseplatform.app;
+/**
+*@author ye
+*@dete 2016年12月6日 下午1:24:53
+*/
+public class AppTest {
+
+}
